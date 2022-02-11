da747d941e881d58887f5b7ca0e7582c5a8358dd
KRGAMING2005 <jagarblixt@gmail.com>
2022-02-11 15:20:25 +0100

    Added remaining servers

--- MinecratServerSetup.py
+++ MinecratServerSetup.py
@@ -39,26 +39,43 @@ def downloadManager(server):
         version = input("What version of Velocity do you want to donwload? ")
         print(f"Downloading velocity-{version}-{getLatesBuild('velocity', version)}.jar")
         fileDownloader(getPaperDownloadURL("velocity", version, getLatesBuild("velocity", version)))
-
     elif server == "Paper":
         version = input("What version of Paper do you want to donwload? ")
         print(f"Downloading paper-{version}-{getLatesBuild('paper', version)}.jar")
         fileDownloader(getPaperDownloadURL("paper", version, getLatesBuild("paper", version)))
+        bungee = input("Do you want this server to be setup as a bungeecord subserver? (y,n)")
+        if bungee == "y":
+            BungeeCordSetup()
+    elif server == "Craftbukkit":
+        version = input("What version of Craftbukkit do you want to download")
+        print(f"Downloading craftbukkit-{version}.jar")
+        fileDownloader(f"https://download.getbukkit.org/craftbukkit/craftbukkit-{version}.jar")
+    elif server == "Spigot":
+        version = input("What version of Spigot do you want to download")
+        print(f"Downloading spigot-{version}.jar")
+        fileDownloader(f"https://download.getbukkit.org/spigot/spigot-{version}.jar")
+        bungee = input("Do you want this server to be setup as a bungeecord subserver? (y,n)")
+        if bungee == "y":
+            BungeeCordSetup()
+    elif server == "BungeeCord":
+        print(f"Downloading BungeeCord.jar")
+        fileDownloader(f"https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar")
+    elif server == "Purpur":
+        version = input("What version of Purpur do you want to download?")
+        print(f"Downloading purpur.jar")
+        fileDownloader(f"https://api.purpurmc.org/v2/purpur/{version}/latest/download")
+        os.rename("download", "purpur.jar")
+        bungee = input("Do you want this server to be setup as a bungeecord subserver? (y,n)")
+        if bungee == "y":
+            BungeeCordSetup()
 
 
+code_to_soft = {"1":"Purpur","2":"Paper","3":"Spigot","4":"Craftbukkit","5":"Velocity","6":"BungeeCord"}
 def translateDownload(software):
-    if software == "6":
-        return "BungeeCord"
-    elif software == "5":
-        return "Velocity"
-    elif software == "4":
-        return "Craftbukkit"
-    elif software == "3":
-        return "Spigot"
-    elif software == "2":
-        return "Paper"
-    elif software == "1":
-        return "Purpur"
+    if software in code_to_soft.keys():
+        return code_to_soft[software]
+    else:
+        return None
 
 
 def menu():
